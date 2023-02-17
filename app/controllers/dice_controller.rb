5837114c8aa6989109263d164327209b682847d9
DTyler0930 <devin.tyler@kellogg.northwestern.edu>
2023-02-17 21:09:44 +0000

    Commit

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,2 +1,11 @@
 class DiceController < ApplicationController
+
+    def index
+      if params["name"]
+        @die1 = rand(1..6)
+        @die2 = rand(1..6)
+        @total = @die1 + @die2
+        @name = params["name"]
+      end
+    end
 end
